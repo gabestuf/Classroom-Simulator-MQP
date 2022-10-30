8c2afd517479f540e41563129e25c002c785002c
Ryan Luu <rmluu@wpi.edu>
2022-10-29 22:34:49 -0400

    updated tilemap generation with randomness onstart

--- tileMapTest.gd
+++ tileMapTest.gd
@@ -4,7 +4,8 @@ class_name TestWorld
 signal started
 signal finished
 
-onready var _tile_map : TileMap = $TileMap
+onready var _tile_map : TileMap = $BorderFloorMap
+onready var _tilemap2 : TileMap = $ObjectObstaclesMap
 
 enum Cell {
 	OBSTACLE
@@ -41,19 +42,27 @@ func generate() -> void:
 func _generate_perimeter() -> void:
 	for x in [0, size.x - 1]:
 		for y in range(0, size.y):
-			_tile_map.set_cell(x,y, 1)
+			if x == 0:
+				_tile_map.set_cell(x,y, 9)
+			else:
+				_tile_map.set_cell(x,y, 10)
+				_tile_map.set_cell(0,9, 4)
+				_tile_map.set_cell(11,9, 5)
+				_tile_map.set_cell(0,0, 6)
+				_tile_map.set_cell(11,0, 7)
 	for x in range(1, size.x - 1):
 		for y in [0, size.y-1]:
 			if y == 0:
 				_tile_map.set_cell(x,y, _pick_random_texture(Cell.OUTER))
 			else:
-				_tile_map.set_cell(x,y, 1)
+				_tile_map.set_cell(x,y, 8)
 
 func _generate_inner() -> void:
 	for x in range(1, size.x-1):
 		for y in range (1, size.y-1):
 			var cell = get_random_tile(ground_probability)
-			_tile_map.set_cell(x,y,cell)
+			_tile_map.set_cell(x,y,11)
+			_tilemap2.set_cell(x,y,_pick_random_texture(Cell.OBSTACLE))
 
 func get_random_tile(probability: float) -> int:
 	return _pick_random_texture(Cell.GROUND) if _rng.randf() < probability else _pick_random_texture(Cell.OBSTACLE)
@@ -64,9 +73,9 @@ func _pick_random_texture(cell_type:int) -> int:
 		if _rng.randf() < window_probability:
 			interval = Vector2(0,0)
 		else:
-			interval = Vector2(1,1)
+			interval = Vector2(3,3)
 	elif cell_type == Cell.GROUND:
 		interval = Vector2(2,2)
 	elif cell_type == Cell.OBSTACLE:
-		interval = Vector2(2,2)
+		interval = Vector2(0,3)
 	return _rng.randi_range(interval.x, interval.y)
