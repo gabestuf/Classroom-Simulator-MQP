13045f3f9d30a27057eae41fab7ee0912105802f
Gabe Camacho <79332351+gabestuf@users.noreply.github.com>
2022-11-01 01:55:06 -0400

    Added config to pass down class hierarchy

--- Simulator.java
+++ Simulator.java
@@ -1,13 +1,29 @@
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class Simulator {
 
     public static void main (String[] args) {
 
+        int x = 16;
+        int y = 3;
+        System.out.println(x / y);
+        //Init Config
+        // init config
+        Properties CONFIG = new Properties();
+        try {
+            final FileInputStream propsInput = new FileInputStream("src/config.properties");
+            CONFIG.load(propsInput);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
         // Init Classroom
-        Classroom classroom = new Classroom();
+        Classroom classroom = new Classroom(CONFIG);
 
         // You can add Tables/Chairs/Doors/Students directly to the room like this
         //classroom.room.addTable(5,5);
-       // classroom.room.addChair(2,2);
+        // classroom.room.addChair(2,2);
         //classroom.room.addDoor(0,1);
 
         // Or easier like this classroom.addChair(2,2)
@@ -19,7 +35,9 @@ public class Simulator {
         // TODO write a function that adds a chair randomly next to a table
 
         // And render like this
-        classroom.render();
+        classroom.renderRoom();
 
     }
 }
+
+
