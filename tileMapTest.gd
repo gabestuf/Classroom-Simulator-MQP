81d7cd60808eab21acce5a043e904d0a3897075c
Ryan Luu <rmluu@wpi.edu>
2022-12-03 22:53:26 -0500

    tile map resizeable from 10,8 to 6,5

--- tileMapTest.gd
+++ tileMapTest.gd
@@ -29,7 +29,6 @@ const CONFIGJSON = {
 }
 
 export (String, FILE, "*.json") var file_path : String
-#export var inner_size := Vector2(10,8)
 #not sure what the acual code is but this is what i did:
 #w = wall
 #x = floor
@@ -40,7 +39,8 @@ export (String, FILE, "*.json") var file_path : String
 #wi = window
 var tiles = [["w", "w", "w", "w", "w", "w", "w"], ["w", "x", "t", "t", "t", "x", "w"], ["w", "x", "t", "t", "t", "x", "w"], ["w", "x", "t", "t", "t", "x", "w"], ["w", "r", "r", "c", "x", "x", "w"], ["w", "r", "r", "x", "c", "x", "w"], ["w", "x", "x", "x", "x", "x", "w"], ["w", "w", "w", "w", "w", "w", "w"]]
 #for now hardcoding these, will eventually use x and y from json file
-export var inner_size := Vector2(6,5)
+export var inner_size := Vector2(10,8)
+#export var inner_size := Vector2(6,5)
 export var perimiter_size := Vector2(1,1)
 export(float, 0, 1) var ground_probability := 0.9
 export(float, 0, 1) var window_probability := 0.2
@@ -93,15 +93,15 @@ func _generate_perimeter() -> void:
 			else:
 				_tile_map.set_cell(x,y, 8)
 # again may not want these hardcoded in the future but for now it's fine
-	_tile_map.set_cell(0,6, 12)
-	_tile_map.set_cell(7,6, 13)
-	_tile_map.set_cell(0,0, 14)
-	_tile_map.set_cell(7,0, 15)
-# what do these 4 set_cells do? - Gabe # They are the four corner tiles - Ryan
-#	_tile_map.set_cell(0,9, 12)
-#	_tile_map.set_cell(11,9, 13)
+#	_tile_map.set_cell(0,6, 12)
+#	_tile_map.set_cell(7,6, 13)
 #	_tile_map.set_cell(0,0, 14)
-#	_tile_map.set_cell(11,0, 15)
+#	_tile_map.set_cell(7,0, 15)
+# what do these 4 set_cells do? - Gabe # They are the four corner tiles - Ryan
+	_tile_map.set_cell(0,9, 12)
+	_tile_map.set_cell(11,9, 13)
+	_tile_map.set_cell(0,0, 14)
+	_tile_map.set_cell(11,0, 15)
 	
 
 func _generate_inner() -> void:
@@ -118,7 +118,7 @@ func _generate_objects() -> void:
 			_tilemap2.set_cell(x,y,_pick_random_texture(Cell.OBSTACLE))
 			_tilemap2.set_cell(1,1,5)
 #			_tilemap2.set_cell(10,1,5)
-#			_tilemap2.set_cell(1,6,7)
+			_tilemap2.set_cell(1,6,7)
 
 func _generate_rugs() -> void:
 	for x in range(7, size.x-1):
@@ -127,14 +127,14 @@ func _generate_rugs() -> void:
 			_tilemap2.set_cellv(Vector2(x,7),3)
 			_tilemap2.set_cellv(Vector2(x,8),3)
 			#set the "obstacles" above it
-			tile = tiles[x][y]
-			match tile:
-				#x will be a transparent tile eventually, overlayed over the floor
-				#"x": _tilemap2.set_cell(x, y, 7)
-				"t": _tilemap2.set_cell(x, y, 4)
-				"r": _tilemap2.set_cell(x, y, 3)
-				"c": _tilemap2.set_cell(x, y, 1)
-				_: print("a perimeter block I think")
+#			tile = tiles[x][y]
+#			match tile:
+#				#x will be a transparent tile eventually, overlayed over the floor
+#				#"x": _tilemap2.set_cell(x, y, 7)
+#				"t": _tilemap2.set_cell(x, y, 4)
+#				"r": _tilemap2.set_cell(x, y, 3)
+#				"c": _tilemap2.set_cell(x, y, 1)
+#				_: print("a perimeter block I think")
 
 
 func get_random_tile(probability: float) -> int:
