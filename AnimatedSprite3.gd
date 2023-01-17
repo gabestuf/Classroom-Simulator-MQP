bc1365e123463582da47dbfc670c8b8e5915dd72
Ryan Luu <rmluu@wpi.edu>
2023-01-16 22:37:35 -0500

    UPDATED as of 1/16, spawn code works and emotions added

--- AnimatedSprite3.gd
+++ AnimatedSprite3.gd
@@ -2,6 +2,6 @@ extends AnimatedSprite
 
 func _ready():
 	# Set the spawn position for the sprite
-	position = Vector2(47,105)
+	position = Vector2(208,175)
 	# Set the animation to play
 	play("default")
