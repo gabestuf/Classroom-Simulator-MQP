953f2f5b2667116800c5ac43f2cdb3553d400825
Ryan Luu <rmluu@wpi.edu>
2023-01-16 21:50:20 -0500

    UPDATED as of 1/16, spawn code works and emotions added

--- AnimatedSprite5.gd
+++ AnimatedSprite5.gd
@@ -7,6 +7,9 @@ var path : = PoolVector2Array() setget set_path
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	position = Vector2(145,105)
+	# Set the animation to play
+	play("default")
 	set_process(false)
 
 
