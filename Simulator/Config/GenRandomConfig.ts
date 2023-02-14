73f05d3792e866b34e90ec54c40ef264df6710d4
gabestuf <gabestuf@gmail.com>
2023-02-14 16:40:59 -0500

    Reduced max size of rooms in genRandomConfig()

--- Simulator/Config/GenRandomConfig.ts
+++ Simulator/Config/GenRandomConfig.ts
@@ -5,11 +5,11 @@ function genRandomConfig(seed = Math.floor(Math.random() * 10000)) {
   const numS = Math.floor(seededRandom(seed * 6) * 4) + 2;
 
   return new ClassroomConfig({
-    roomSizeX: Math.floor(seededRandom(seed * 2) * 10) + 8,
-    roomSizeY: Math.floor(seededRandom(seed + 3) * 10) + 8,
+    roomSizeX: Math.floor(seededRandom(seed * 2) * 10) + 2,
+    roomSizeY: Math.floor(seededRandom(seed + 3) * 10) + 2,
     numStudents: numS,
     numTeachers: Math.floor(seededRandom(seed * 1.5) * 1) + 1,
-    numChairs: numS,
+    numChairs: numS + 1,
     numTables: Math.floor(seededRandom(seed * 7) * 3) + 1,
     numRugs: Math.floor(seededRandom(seed * 5) * 2) + 1,
     seed: seed,
