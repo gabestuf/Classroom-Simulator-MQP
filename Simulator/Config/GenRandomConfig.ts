7074309982492bc8d1ef65bb674b27e5ce81a046
Gabe Camacho <gabestuf@gmail.com>
2023-03-22 17:34:28 -0400

    made random room sizes base slightly larger

--- Simulator/Config/GenRandomConfig.ts
+++ Simulator/Config/GenRandomConfig.ts
@@ -5,8 +5,8 @@ function genRandomConfig(seed = Math.floor(Math.random() * 10000)) {
   const numS = Math.floor(seededRandom(seed * 6) * 4) + 2;
 
   return new ClassroomConfig({
-    roomSizeX: Math.floor(seededRandom(seed * 2) * 10) + 5,
-    roomSizeY: Math.floor(seededRandom(seed + 3) * 10) + 5,
+    roomSizeX: Math.floor(seededRandom(seed * 2) * 10) + 7,
+    roomSizeY: Math.floor(seededRandom(seed + 3) * 10) + 7,
     numStudents: numS,
     numTeachers: Math.floor(seededRandom(seed * 1.5) + 1), // change to 2 if we want max 2 teachers
     numChairs: numS + 1,
